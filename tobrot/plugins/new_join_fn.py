9c77d1d7b518de4bcd99a67465bffc94e8b1c9e0
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2020-09-03 20:40:21 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -35,7 +35,7 @@ async def new_join_f(client, message):
 async def help_message_f(client, message):
     # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
     channel_id = str(AUTH_CHANNEL)[4:]
-    message_id = 99
+    message_id = 277
     # display the /help message
     await message.reply_text(
         f"please read the <a href='https://t.me/c/{channel_id}/{message_id}'>Pinned Message</a>",
@@ -48,12 +48,12 @@ async def rename_message_f(client, message):
     inline_keyboard.append([
         pyrogram.InlineKeyboardButton(
             text="read this?",
-            url="https://t.me/keralagram/698909"
+            url="https://t.me/AI_bot_projects/3"
         )
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
     await message.reply_text(
-        "please use @renamebot",
+        "please use @TGrenamebyAI_bot",
         quote=True,
         reply_markup=reply_markup
     )
